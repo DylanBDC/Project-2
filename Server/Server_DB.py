ac41cd1cf81cb06c9f9e0c7988b0f7480bc7d78e
DylanBDC <dylan.brett@dcmail.ca>
2024-12-08 17:27:18 -0500

    Added iterations
    In this commit I have added iterations (outputs what data set it is on) I have also started trying to add the GUI but it isn't working properly yet.

--- Server/Server_DB.py
+++ Server/Server_DB.py
@@ -14,6 +14,7 @@
 
 import socket
 import json
+import PySimpleGUI as sg
 import time
 
 
@@ -25,41 +26,59 @@ s.bind((host, port))
 s.listen(5)
 print("server active ctrl-c to shut down") # displays if the server has been turned on
 
-# try to connect to server (if it doesnt connect it will close and print lost connection)
-try:
-    c, addr = s.accept()
-    print ('Got connection from',addr)
-    while True:
-        encoded_string = c.recv(1024)
-        decoded_string = encoded_string.decode('utf-8') # decode the string
-        data = json.loads(decoded_string) # converts the json string into a python object
-        # access the dictionary values
-        core = data["Temperature"]
-        volts = data["Voltage"]
-        core_clock = data["core-clock"]
-        Gpu_clock = data["GPU-Clock"]
-        video_voltage = data["Video-voltage"]
+# sg.theme('DarkAmber') # Add a touch of color
+# # All the stuff inside your window.
+# layout = [	 [sg.Text('Central Terminal')], # change text to ask the user to enter a date
+#             [sg.Text('Core Temperature:'), sg.Text(f"Core Temperature: {data['Temperature']}°C")], # show the user the desired date format
+#             [sg.Button('Ok'), sg.Button('Cancel')] ]
+# Create the Window
 
-        # print out the real time data
-        #print(encoded_string) # shows the raw data coming in
-        print("Core Temperature:", core)
-        print("Core Voltage:", volts, "V")
-        print("Core Clock:", core_clock, "GHz")
-        print("GPU Core Clock:", Gpu_clock, "GHz")
-        print("Video Core Voltage:", video_voltage, "V")
-        print("") # added a space between updates
 
-except ConnectionResetError: # if the client disconnects then the program will stop sending data
-    print("the client has disconnected")
-    c.close()
-except BrokenPipeError: # when the client disconnects using ctrl-c will stop sending the data
-    print("the client has disconnected")
-    c.close()
-except KeyboardInterrupt: # press ctrl-c to exit the program
-    print("")
-    print("Server Shutting down")
-    c.close()
-    exit(1)
+
+while True:
+#     c, addr = s.accept()
+#     print ('Got connection from',addr)
+    #window = sg.Window('Central Guard Station', layout) # added my name to the title
+    try:
+        c, addr = s.accept()
+        print ('Got connection from',addr)
+        while True:
+            try:
+                encoded_string = c.recv(1024)
+                decoded_string = encoded_string.decode('utf-8') # decode the string
+                data = json.loads(decoded_string) # converts the json string into a python object
+                # access the dictionary values
+                core = data["Temperature"]
+                volts = data["Voltage"]
+                core_clock = data["core-clock"]
+                Gpu_clock = data["GPU-Clock"]
+                video_voltage = data["Video-voltage"]
+                iterations = data["Iterations"]
+
+                # print out the real time data
+                #print(encoded_string) # shows the raw data coming in
+                print("Core Temperature:", core)
+                print("Core Voltage:", volts, "V")
+                print("Core Clock:", core_clock, "GHz")
+                print("GPU Core Clock:", Gpu_clock, "GHz")
+                print("Video Core Voltage:", video_voltage, "V")
+                print(iterations)
+                print("") # added a space between updates
+            except:
+                print("all data sent")
+                break
+
+    except ConnectionResetError: # if the client disconnects then the program will stop sending data
+        print("the client has disconnected")
+        c.close()
+    except BrokenPipeError: # when the client disconnects using ctrl-c will stop sending the data
+        print("the client has disconnected")
+        c.close()
+    except KeyboardInterrupt: # press ctrl-c to exit the program
+        print("")
+        print("Server Shutting down")
+        c.close()
+        exit(1)
         
         
 
